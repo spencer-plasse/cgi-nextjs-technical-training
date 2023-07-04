5b626c8ad9d88fdb70be8dd4595811cd8cdb8248
Spencer Plasse <spencer.plasse@cgi.com>
2023-07-04 17:38:07 -0500

    Added temporary comments with links on how to fix Redux integration

--- utils/redux/store.ts
+++ utils/redux/store.ts
@@ -2,6 +2,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import apiReducer from './apiSlice'
 
+// TODO: https://stackoverflow.com/questions/61281070/nextjs-preload-redux-data-in-getserversideprops
+// https://github.com/vercel/next.js/blob/canary/examples/with-redux-wrapper/pages/other.js
 export const store = configureStore({
   reducer: {
     api: apiReducer
